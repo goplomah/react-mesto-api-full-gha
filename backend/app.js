f90eedb07133cc7387c036582df16c55d363405b
Maks Subochev <maxim.subochev@yandex.ru>
2023-07-16 21:58:02 +0300

    Изменён адрес подключения к базе данных

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { PORT = 3000 } = process.env;
 
 mongoose
-  .connect('mongodb://localhost:27017/mestodb ', {
+  .connect('mongodb://127.0.0.1:27017/mestodb ', {
     useNewUrlParser: true,
   })
   .then(() => {
